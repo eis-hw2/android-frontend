7fadaab2e8dbb1714b959edb766f4d49df961d0b
ssingularity <1755405701@qq.com>
2019-04-11 23:22:10 +0800

    successfully add barer

--- app/src/main/java/com/pipipan/demo/helper/GlideImageLoader.java
+++ app/src/main/java/com/pipipan/demo/helper/GlideImageLoader.java
@@ -2,15 +2,14 @@ package com.pipipan.demo.helper;
 
 
 import android.content.Context;
-import android.net.Uri;
 import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.youth.banner.loader.ImageLoader;
 
 public class GlideImageLoader extends ImageLoader {
     @Override
     public void displayImage(Context context, Object path, ImageView imageView) {
-        Uri uri = Uri.parse((String) path);
-        imageView.setImageURI(uri);
+        Glide.with(context).load(path).into(imageView);
     }
 }
