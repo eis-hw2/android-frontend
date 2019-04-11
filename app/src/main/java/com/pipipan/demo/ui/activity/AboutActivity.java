714e9a03b32b78bc8fb24717ff51f68da146bf7e
ssingularity <1755405701@qq.com>
2019-04-11 15:18:35 +0800

    initialize the project

---
+++ app/src/main/java/com/pipipan/demo/ui/activity/AboutActivity.java
@@ -0,0 +1,33 @@
+package com.pipipan.demo.ui.activity;
+
+import com.pipipan.demo.R;
+import com.pipipan.demo.common.MyActivity;
+
+/**
+ *    author : Android 轮子哥
+ *    github : https://github.com/getActivity/AndroidProject
+ *    time   : 2018/10/18
+ *    desc   : 关于界面
+ */
+public class AboutActivity extends MyActivity {
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_about;
+    }
+
+    @Override
+    protected int getTitleBarId() {
+        return R.id.tb_about_title;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
---
+++ app/src/main/java/com/pipipan/demo/ui/activity/CopyActivity.java
@@ -0,0 +1,33 @@
+package com.pipipan.demo.ui.activity;
+
+import com.pipipan.demo.R;
+import com.pipipan.demo.common.MyActivity;
+
+/**
+ *    author : Android 轮子哥
+ *    github : https://github.com/getActivity/AndroidProject
+ *    time   : 2018/10/18
+ *    desc   : 可进行拷贝的副本
+ */
+public class CopyActivity extends MyActivity {
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_copy;
+    }
+
+    @Override
+    protected int getTitleBarId() {
+        return R.id.tb_copy_title;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
